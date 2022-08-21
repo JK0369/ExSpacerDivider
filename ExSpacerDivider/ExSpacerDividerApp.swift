99db1e159854ec78062a647cc86c29677daf83c5
jake <palatable7@naver.com>
2022-08-21 21:48:01 +0900

    Initial Commit

---
+++ ExSpacerDivider/ExSpacerDividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExSpacerDividerApp.swift
+//  ExSpacerDivider
+//
+//  Created by 김종권 on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ExSpacerDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
